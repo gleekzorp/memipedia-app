652e50b040fabfb3db2091ad9f59ea6a3271349b
Daniel Floyd <gleek.dan@gmail.com>
2020-07-01 16:09:00 -0600

    Navigating Between Screens in React Native

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 
 import router from './utils/router';
 import { createAppContainer } from 'react-navigation';
@@ -7,17 +6,6 @@ const AppContainer = createAppContainer(router);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppContainer />
-    </View>
+    <AppContainer />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 
-export default () => {
+export default (props) => {
   return (
     <View>
       <Text>Feed Screen</Text>
+
+      <TouchableOpacity onPress={() => props.navigation.navigate("Search")}>
+        <Text>
+          Search
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
