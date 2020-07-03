66d1ebcf27e61abe0337d1af129af62051d1972b
Daniel Floyd <gleek.dan@gmail.com>
2020-07-03 17:47:46 -0600

    Creating a Custom Button Component with the Props Spread Process

--- screens/auth/AuthScreen.tsx
+++ screens/auth/AuthScreen.tsx
@@ -1,75 +1,85 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
 
-import textInputStyles from '../../styles/forms/textInputStyles';
-const {textFieldWrapper, textField} = textInputStyles
-import authScreenStyles from '../../styles/stacks/auth/authScreenStyles';
-import API from '../../utils/api';
+import textInputStyles from "../../styles/forms/textInputStyles";
+const { textFieldWrapper, textField } = textInputStyles;
+import authScreenStyles from "../../styles/stacks/auth/authScreenStyles";
+import API from "../../utils/api";
+import Button from "../../components/helpers/Button";
 
 interface IAuthScreenProps {
   navigation: {
     navigate: (arg: string) => void;
-  }
+  };
 }
-export default (props : IAuthScreenProps) => {
+export default (props: IAuthScreenProps) => {
   const [formToShow, setFormToShow] = useState("LOGIN");
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const screenTypeText = () => {
     if (formToShow === "LOGIN") {
-      return "Need an account?  Register";
+      return "Need an account? Register";
     } else if (formToShow === "REGISTER") {
-      return "Already have an account?  Login";
+      return "Already have an account? Login";
     }
   };
-  
+
   const handleAuthTypePress = () => {
     if (formToShow === "LOGIN") {
-      setFormToShow("REGISTER")
+      setFormToShow("REGISTER");
     } else if (formToShow === "REGISTER") {
-      setFormToShow("LOGIN")
+      setFormToShow("LOGIN");
     }
   };
-  
+
   const headerText = () => {
     if (formToShow === "LOGIN") {
-      return "Login"
+      return "Login";
     } else if (formToShow === "REGISTER") {
-      return "Register"
+      return "Register";
     }
   };
 
   const handleSubmit = () => {
+    setIsSubmitting(true);
+
     const params = {
       auth: {
         email: email,
         password: password
       }
-    }
-    API
-      .post("memipedia_user_token", params)
+    };
+    API.post("memipedia_user_token", params)
       .then(response => {
         if (response.data.jwt) {
-          props.navigation.navigate("Feed")
+          props.navigation.navigate("Feed");
         } else {
-          alert('It looks like you typed in the wrong email or password, please try again')
+          alert(
+            "It looks like you typed in the wrong email or password, please try again"
+          );
         }
+
+        setIsSubmitting(false);
       })
       .catch(error => {
-        alert('It looks like you typed in the wrong email or password, please try again')
-      })
+        setIsSubmitting(false);
+        alert(
+          "It looks like you typed in the wrong email or password, please try again"
+        );
+      });
   };
 
   return (
     <View style={authScreenStyles.container}>
-      <Text style={{color: "white"}}>{headerText()}</Text>
+      <Text style={{ color: "white" }}>{headerText()}</Text>
 
       <View style={textFieldWrapper}>
         <TextInput
-          placeholder="Email..."
+          placeholder="Email"
           value={email}
-          onChangeText={(val) => setEmail(val)}
+          onChangeText={val => setEmail(val)}
           style={textField}
           autoCapitalize="none"
           spellCheck={false}
@@ -78,21 +88,23 @@ export default (props : IAuthScreenProps) => {
 
       <View style={textFieldWrapper}>
         <TextInput
-          placeholder="Password..."
+          placeholder="Password"
           value={password}
-          onChangeText={(val) => setPassword(val)}
+          onChangeText={val => setPassword(val)}
           style={textField}
           secureTextEntry={true}
         />
       </View>
 
       <TouchableOpacity onPress={handleAuthTypePress}>
-        <Text style={{color: "white"}}>{screenTypeText()}</Text>
+        <Text style={{ color: "white" }}>{screenTypeText()}</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={handleSubmit}>
-        <Text style={{color: "white"}}>{headerText()}</Text>
-      </TouchableOpacity>
+      {isSubmitting ? (
+        <Button text={"Submitting..."} onPress={handleSubmit} disabled={true} />
+      ) : (
+        <Button text={headerText()} onPress={handleSubmit} />
+      )}
     </View>
-  )
-}
+  );
+};
