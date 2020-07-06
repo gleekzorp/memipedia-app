c5069e6f3a00125fe5c96202b0a73d0397291141
Daniel Floyd <gleek.dan@gmail.com>
2020-07-06 16:27:59 -0600

    Building the Check Login API Call

--- screens/auth/AuthLoadingScreen.tsx
+++ screens/auth/AuthLoadingScreen.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useContext} from 'react';
 import { View } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import CurrentUserContext from '../../contexts/CurrentUserContext';
+import api from '../../utils/api';
 
 
 interface IauthLoadingScreenProps {
@@ -23,9 +24,29 @@ export default (props: IauthLoadingScreenProps) => {
     console.log('in check login in the auth loading screen')
 
     if (token) {
-      // TODO: call API to ensure logged in
+      api
+        .get("logged_in", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        .then(response => {
+          console.log('response from checkLogin', response.data);
+          
+          if (response.data.memipedia_user) {
+            setCurrentUser(response.data.memipedia_user);
+            props.navigation.navigate('App');
+          } else {
+            setCurrentUser(null);
+            props.navigation.navigate('Auth');
+          }
+        })
+        .catch((error) => {
+          setCurrentUser(null);
+          props.navigation.navigate('Auth');
+        })
     } else {
-      setCurrentUser(null)
+      setCurrentUser(null);
       props.navigation.navigate('Auth');
     }
   }
