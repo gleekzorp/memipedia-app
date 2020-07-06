0bbfef785b5c3cba5ecd95e9a67405133fc6e298
Daniel Floyd <gleek.dan@gmail.com>
2020-07-06 15:43:03 -0600

    Building the Initial Auth Loading Screen

---
+++ screens/auth/AuthLoading.tsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useContext} from 'react';
+import { View } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
+
+
+interface IauthLoadingScreenProps {
+  navigation: {
+    navigate: (screenName: string) => string
+  }
+}
+
+export default (props: IauthLoadingScreenProps) => {
+  const { setCurrentUser } = useContext(CurrentUserContext);
+
+  useEffect(() => {
+    checkLogin()
+  }, [])
+
+  const checkLogin = async () => {
+    const token = await SecureStore.getItemAsync("memipedia_secure_token");
+
+    if (token) {
+      // TODO: call API to ensure logged in
+    } else {
+      setCurrentUser(null)
+      props.navigation.navigate('Auth');
+    }
+  }
+
+  return <View />
+}
