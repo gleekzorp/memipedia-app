e39620f9594a27ce39096f8b64267ed4408e2389
Daniel Floyd <gleek.dan@gmail.com>
2020-07-07 15:13:55 -0600

    Introduction to the Expo ImagePicker Component

--- screens/PostFormScreen.tsx
+++ screens/PostFormScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import PostImagePicker from '../components/posts/PostImagePicker';
 
 export default () => {
   return (
-    <View>
+    <View style={{height: "100%"}}>
       <Text>Post Form Screen</Text>
+      <View style={{marginTop: 40, height: "100%"}}>
+        <PostImagePicker />
+      </View>
     </View>
   )
 }
