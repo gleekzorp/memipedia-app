40be75ab5bade13e6b1f1ccb20f641b886b64992
Daniel Floyd <gleek.dan@gmail.com>
2020-07-07 16:53:24 -0600

    Refactoring the ImagePicker Component

--- screens/PostFormScreen.tsx
+++ screens/PostFormScreen.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, Text } from 'react-native';
 import PostImagePicker from '../components/posts/PostImagePicker';
 import Button from '../components/helpers/Button';
 
 export default () => {
   const [name, setName] = useState("")
   const [content, setContent] = useState("")
+  const [postImage, setPostImage] = useState(null)
 
   return (
     <View style={{height: "100%"}}>
@@ -22,12 +23,15 @@ export default () => {
         multiline
       />
       <View style={{marginTop: 40, height: 100}}>
-        <PostImagePicker />
+        <PostImagePicker setPostImage={setPostImage} />
       </View>
       <Button
         text="Submit"
         onPress={() => console.log('submitting...')}
       />
+      <View>
+        <Text>{postImage ? postImage : null}</Text>
+      </View>
     </View>
   )
 }
